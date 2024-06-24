f436f3c41ae174613bcf80f04b984b9a44360b94
Dart <Danslis@yandex.ru>
2024-06-24 20:39:39 +0300

    Кастомный контрол, Control Value Accessor

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,19 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormsComponent } from './components/forms/forms.component';
 import { FormsRouting } from './forms-routing.module';
+import { RateComponent } from './components/rate/rate.component';
 
 
 
 @NgModule({
   declarations: [
-    FormsComponent
+    FormsComponent,
+    RateComponent
   ],
   imports: [
     CommonModule,
     FormsRouting,
     ReactiveFormsModule,
+    FormsModule,
   ]
 })
-export class FormsModule { }
+export class FormsUIModule { }
